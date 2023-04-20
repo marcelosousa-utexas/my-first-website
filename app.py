9db191d59e1fdf9e07d64f8d24998fde86b272ca
marcelosousa-utexas <112403984+marcelosousa-utexas@users.noreply.github.com>
2023-04-20 15:21:36 +0000

    Add image, a new html file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 class flask_interface():
   app = Flask(__name__)
   
   @app.route("/")
   def hello_word():
-    return "Hello, world"
+    return render_template("home.html")
     #return "<p> Hello, world <p>"
 
   #if __name__ == "__name__":  
